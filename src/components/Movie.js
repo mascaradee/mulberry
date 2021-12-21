5587e058d8c89437a4ada2eb7f9018661ddde16d
mascaradee <lamascaradee@gmail.com>
2021-12-21 22:59:10 +0900

    영화 설명이 너무 길어서 기준 글자 이상이면 정리

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -18,7 +18,7 @@ function Movie({ id, coverImg, url, title, summary, genres }) {
         <Link to={`/movie/${id}`}>{title}</Link>
         {/* <a href="/movie">{title}</a> */}
       </h1>
-      <p>{summary}</p>
+      <p>{summary.length > 235 ? `${summary.slice(0, 235)}...` : summary}</p>
       <ul>
         {genres.map((g) => (
           <li key={g}>{g}</li>
